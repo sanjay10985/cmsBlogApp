49b486a42204fe8ec8b8c9971c381714e75df37d
sanjay10985 <sanjaytomar717@gmail.com>
2022-10-12 23:17:13 +0530

    "The project is done finally"

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import {PostCard,Categories,PostWidget} from '../components'
+import {PostCard,Categories,PostWidget, FeaturedPostCard} from '../components'
 import {getPosts} from '../services'
-
+import {FeaturedPosts} from '../sections'
 
 const Home = ({posts}) => {
   return (
@@ -10,6 +10,7 @@ const Home = ({posts}) => {
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts/>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
         <div className='lg:col-span-8 col-span-1'>
         {posts.map((post,index)=>(
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -47,6 +47,6 @@ export async function getStaticPaths()
 
     return{
         paths: posts.map(({node:{slug}}) => ({params: {slug}})),
-        fallback: false,
+        fallback: true,
     };
 }
